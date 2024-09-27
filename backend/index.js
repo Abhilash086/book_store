2151c8718e29e3fc9f226a2f8235d0d62859a410
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-09-27 15:54:29 +0530

    installed mongoose and connected to DB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
-import { PORT } from './config.js';
+import mongoose from 'mongoose';
+import { PORT, mongoDBURL } from './config.js';
 
 const app = express();
 
-app.listen(PORT, ()=>{
-    console.log(`Server running at PORT ${PORT}`);
+app.get("/",(req, res)=>{
+    return res.status(200).send("Welcome default");
 });
+
+mongoose.connect(mongoDBURL)
+.then(()=>{
+    console.log("App connected to database");
+    app.listen(PORT, ()=>{
+        console.log(`Server running at PORT ${PORT}`);
+    });
+}).catch((error)=>{
+    console.log(error);
+})
 
