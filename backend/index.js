835edd48d76f13dd7147a20353b523576d0503ca
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-09-29 23:51:15 +0530

    implemented PUT method to Update a book in database by ID

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.post("/books",async (req,res)=>{
                 message: "Send all required fields: title, author and publishYear"
             })
         }
-
+        
         const newBook = {
             title: req.body.title,
             author: req.body.author,
@@ -60,7 +60,34 @@ app.post("/books",async (req,res)=>{
         console.log(error.message);
         return res.status(500).send({message: error.message});
     }
-})
+});
+
+// Route for Update a Book
+app.put("/books/:id",async (req,res)=>{
+    try {
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ){
+            return res.status(400).send({
+                message: "Send all required fields: title, author and publishYear"
+            });
+        }
+        const {id} = req.params;
+        
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if(!result){
+            return res.status(404).json({message: "Book not found"});
+        }
+        
+        return res.status(200).send("Book Updated Successfully");
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).send({message: error.message});
+    }
+});
 
 mongoose.connect(mongoDBURL)
 .then(()=>{
