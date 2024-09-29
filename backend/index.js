9b0a6a6922d93f07eba4d58e22c9bf5cb7cea400
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-09-29 23:02:22 +0530

    Route created for GET book by ID

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ app.get("/",(req, res)=>{
     return res.send("Welcome default")
 });
 
+// Route for GET all books from Database
 app.get("/books",async (req,res)=>{
     try {
         const books = await Book.find({});
@@ -23,8 +24,22 @@ app.get("/books",async (req,res)=>{
         console.log(error.message);
         return res.status(500).send({message: error.message});
     }
-})
+});
 
+// Route for GET a specific book by ID
+app.get("/books/:id",async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const book = await Book.findById(id);
+
+        return res.status(200).send(book);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).send({message: error.message});
+    }
+});
+
+// Route to create book in books-collection Database
 app.post("/books",async (req,res)=>{
     try {
         if(!req.body.title || !req.body.author || !req.body.publishYear){
