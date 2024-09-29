b39b7681038575e251c4766edb82053e0191303b
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-09-29 22:17:04 +0530

    Added bookModel.js and Post method created

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,37 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookmodel.js';
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/",(req, res)=>{
     return res.send("Welcome default")
 });
+
+app.post("/books",async (req,res)=>{
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            return res.status(400).send({
+                message: "Send all required fields: title, author and publishYear"
+            })
+        }
+
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        }
+
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).send({message: error.message});
+    }
+})
 
 mongoose.connect(mongoDBURL)
 .then(()=>{
@@ -15,5 +40,5 @@ mongoose.connect(mongoDBURL)
         console.log(`Server running at port: ${PORT}`)
     });
 }).catch((error)=>{
-    console.log({message: error.message});
+    console.log(error);
 })
