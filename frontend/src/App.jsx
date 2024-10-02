73da281f119b8673d5250ab74cc1c71dbbdd15a3
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-10-02 22:02:14 +0530

    Added pages folder in frontend and installed packages axios,react-router-dom and react-icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
+import CreateBooks from './pages/CreateBooks'
+import ShowBook from './pages/ShowBook'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/books/create' element={<CreateBooks/>}></Route>
+      <Route path='/books/details/:id' element={<ShowBook/>}></Route>
+      <Route path='/books/edit/:id' element={< EditBook/>}></Route>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}></Route>
+    </Routes>
   )
 }
 
