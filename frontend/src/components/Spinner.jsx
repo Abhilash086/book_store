e2d206f94856cdd6eebea3ead6c7b7b93de6f9a9
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-10-03 22:31:24 +0530

    Updated Spinner and modified operations

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-500'></div>
+    // <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-500'></div>
+    <div className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-50 z-50">
+            <div className="animate-spin rounded-full border-t-4 border-b-4 border-blue-500 h-16 w-16"></div>
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             });
     },[]);
   return (
-    <div className="p-4 ">
+    <div className="p-4">
         <div className="flex justify-between items-center">
             <h1 className="text-3xl my-8">Books List</h1>
             <Link to="/books/create">
@@ -58,7 +58,7 @@ const Home = () => {
                             <td className="border border-slate-700 rounded-md text-center max-md:hidden">
                                 {book.publishYear}
                             </td>
-                            <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                            <td className="border border-slate-700 rounded-md text-center">
                                 <div className="flex justify-center gap-x-4">
                                     <Link to={`/books/details/${book._id}`}>
                                         <BsInfoCircle className="text-2xl text-green-800"/>
